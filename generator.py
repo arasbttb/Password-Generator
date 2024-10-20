0039fe5e159ee80b62c936c0dc778010a40b31e3
arasbttb <einstenokul@gmail.com>
2024-10-20 20:19:54 +0300

    Create generator.py
    Strong password generator.

---
+++ generator.py
@@ -0,0 +1,27 @@
+import random
+password1=random.randint(1,9)
+password2=random.randint(1,9)
+password3=random.randint(1,9)
+password4=random.randint(1,9)
+password5=random.randint(1,9)
+password6=random.randint(1,9)
+password7=random.randint(1,9)
+password8=random.randint(1,9)
+password9=random.randint(1,9)
+basamak=random.randint(1,9)
+if basamak == 2:
+  print(password1,password2)
+if basamak == 3:
+  print(password1,password2,password3)
+if basamak == 4:
+  print(password1,password2,password3,password4)
+if basamak == 5:
+  print(password1,password2,password3,password4,password5)
+if basamak == 6:
+  print(password1,password2,password3,password4,password5,password6)
+if basamak == 7:
+  print(password1,password2,password3,password4,password5,password6,password7)
+if basamak == 8:
+  print(password1,password2,password3,password4,password5,password6,password7,password8)
+if basamak == 9:
+  print(password1,password2,password3,password4,password5,password6,password7,password8,password9)
